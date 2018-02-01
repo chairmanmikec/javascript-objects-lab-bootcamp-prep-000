b4f1e9094b53de3892f58234ca80a226d4639760
chairmanmikec <michaelc1012@gmail.com>
2018-02-01 17:35:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,5 +5,21 @@ var recipes = {
 }
 
 function updateObjectWithKeyAndValue (object,key,value){
-  Object.assign({},object,{[key]:value})
+  return Object.assign({},object,{[key]:value})
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key] = value
+  return object
+}
+
+function deleteFromObjectByKey (object,key){
+  var newObj = Object.assign({},object)
+  delete newObj[key]
+  return newObj
+}
+
+function destructivelyDeleteFromOBjectByKey(object,key){
+  delete object[key]
+  return object
 }
